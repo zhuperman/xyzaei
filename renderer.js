77755da1b050464b8c9fe5fef8da5cbbcc723e41
Johnson Zhu <johnsonzhuuu@gmail.com>
2022-12-04 13:49:56 -0500

    adding arrow key scrolling

--- renderer.js
+++ renderer.js
@@ -17,17 +17,37 @@ let sortAttribute = RELEASE_DATE;
 let sortDirection = -1;
 
 document.addEventListener('keydown', async function(event){
+  // Exit
   if (event.keyCode == 27) {
     window.launcher.exit();
   }
+
+  // Arrow Key Scrolling
+  if (event.keyCode == 37) {
+    if (currentItemId > 0) {
+      let prevItemId = currentItemId;
+      currentItemId -= 1;
+      if (itemsPerPage > 1) { highlightCurrentItem(prevItemId); }
+    }
+    if (currentItemId < items.length - 1) { window.dispatchEvent(new WheelEvent('wheel', {'deltaY': -1})); }
+  } else if (event.keyCode == 39) {
+    if (currentItemId < items.length - 1) {
+      let prevItemId = currentItemId;
+      currentItemId += 1;
+      if (itemsPerPage > 1) { highlightCurrentItem(prevItemId); }
+    }
+    if (currentItemId > 0) { window.dispatchEvent(new WheelEvent('wheel', {'deltaY': 1})); }
+  }
+
+  // Item Type
   if (event.keyCode == 112) {
     itemType = (itemType == GAME ? MOVIE : GAME);
     await init();
   }
+  // Sort Type
   if (event.keyCode == 113) {
     sortAttribute = sortAttribute == RELEASE_DATE ? TITLE : RELEASE_DATE;
     sortDirection = sortAttribute == RELEASE_DATE ? -1 : 1;
-    console.log(sortAttribute, sortDirection);
     await init();
   }
 });
@@ -49,27 +69,38 @@ let logos;
 let ultrawide = false;
 let ultrawideBuffer = 60;
 
+const COVER_OPACITY = 0.8;
+const LOGO_OPACITY = 0.9;
+let currentItemId;
+
 async function loadItems() {
   let jsonData = await window.items.fetch(itemType);
 
-  return jsonData.sort((a, b) => { return sortDirection * a[sortAttribute].localeCompare(b[sortAttribute]); }).map(itemData => {
+  return jsonData.sort((a, b) => { return sortDirection * a[sortAttribute].localeCompare(b[sortAttribute]); }).map((itemData, index) => {
     let item = document.createElement("div");
     item.className = `item ${itemType}`;
+    item.id = index;
     item.style.width = `${itemWidth}px`;
     item.dataset.target = itemData.target;
+
     let cover = document.createElement("div");
     cover.className = `cover ${itemType}`;
     cover.style.backgroundImage = `url("${itemData.cover.replace(/\\/g, "/")}")`;
+    cover.style.opacity = COVER_OPACITY / 2;
     item.appendChild(cover);
+
     if (itemData.logo) {
       let logo = document.createElement("div");
       logo.className = `logo ${itemType}`;
       logo.style.backgroundImage = `url("${itemData.logo.replace(/\\/g, "/")}")`;
+      logo.style.opacity = LOGO_OPACITY / 2;
       item.appendChild(logo);
     }
-    item.addEventListener('click', launchItem);
-    sliderInner.appendChild(item);
 
+    if (itemsPerPage > 1) item.addEventListener('mouseover', updateCurrentItem);
+    item.addEventListener('click', launchItem);
+
+    sliderInner.appendChild(item);
     return item;
   });
 }
@@ -116,7 +147,7 @@ async function init() {
     if (itemType == GAME) {
       cover.style.left = `-${(itemWidth + itemBufferWidth) * (idx + (idx == 0 && !ultrawide ? 1 : 0)) + (window.outerHeight * 16 / 9 - window.outerHeight * mainAspectRatio) / 2 + (ultrawide ? ultrawideBuffer / 2 : 0)}px`;
     }
-    if (itemType == MOVIE && itemsPerPage == 1) { cover.style.opacity = 0.8; }
+    if (itemType == MOVIE && itemsPerPage == 1) { cover.style.opacity = COVER_OPACITY; }
     cover.style.backgroundSize = ultrawide ? 'cover' : 'contain';
   });
 
@@ -124,7 +155,29 @@ async function init() {
     logo.style.left = `${itemWidth * 0.125}px`;
   });
 
+  currentItemId = Math.floor(itemsPerPage / 2);
+  highlightCurrentItem(currentItemId);
+
   document.body.style.height = `${sliderWidth - (window.innerWidth - window.innerHeight) + remainingWidth}px`;
+}
+
+function updateCurrentItem(event) {
+  let prevItemId = currentItemId;
+  currentItemId = parseInt(event.target.parentElement.id);
+  highlightCurrentItem(prevItemId);
+}
+
+function highlightCurrentItem(prevItemId) {
+  let prevItem = document.getElementById(prevItemId.toString());
+  for (let child of prevItem.children) {
+    if (child.classList.contains('cover')) { child.style.opacity = COVER_OPACITY / 2; }
+    if (child.classList.contains('logo')) { child.style.opacity = LOGO_OPACITY / 2; }
+  }
+  let currentItem = document.getElementById(currentItemId.toString());
+  for (let child of currentItem.children) {
+    if (child.classList.contains('cover')) { child.style.opacity = COVER_OPACITY; }
+    if (child.classList.contains('logo')) { child.style.opacity = LOGO_OPACITY; }
+  }
 }
 
 function launchItem(event) {
